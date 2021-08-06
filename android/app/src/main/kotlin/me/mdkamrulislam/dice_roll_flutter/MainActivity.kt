80758db4053fb25c9a96202330375cb1fe4b98ac
mdkamrulislam-web <md.kamrul.islam@g.bracu.ac.bd>
2021-08-06 20:52:53 +0600

    Initial Commit

---
+++ android/app/src/main/kotlin/me/mdkamrulislam/dice_roll_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package me.mdkamrulislam.dice_roll_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dice Roll'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 5;
+  int rightDiceNumber = 2;
+
+  void changeDiceFace() {
+    leftDiceNumber = Random().nextInt(6) + 1; // 1-6
+    rightDiceNumber = Random().nextInt(6) + 1; //1-6
+  }
+
+  //To make the variable only once, when the 1st time the dice page gets created
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    //changeDiceFace(); //Function Call
+                  });
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset('images/dice$leftDiceNumber.png'),
+                ),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    //changeDiceFace(); //Function Call
+                  });
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset('images/dice$rightDiceNumber.png'),
+                ),
+              ),
+            ),
+          ],
+        ),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.red[900],
+            shadowColor: Colors.red[500],
+            elevation: 5.0,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            onSurface: Colors.blue,
+            padding: EdgeInsets.all(20.0),
+          ),
+          onPressed: () {
+            setState(() {
+              changeDiceFace(); //Function Call
+            });
+          },
+          child: Text(
+            "ROLL!",
+            style: TextStyle(
+              fontSize: 25.0,
+              letterSpacing: 3.0,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
